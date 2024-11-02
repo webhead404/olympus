30e598b9ac9af733172849d0b8d511e76d3b8742
webhead404 <kvvincentvalentine@gmail.com>
2024-11-02 11:32:27 -0500

    fixing more env vars

--- vagrant/scripts/elastic/install-vectr.sh
+++ vagrant/scripts/elastic/install-vectr.sh
@@ -8,6 +8,7 @@ mkdir -p /opt/vectr
 cd /opt/vectr
 wget https://github.com/SecurityRiskAdvisors/VECTR/releases/download/ce-8.9.2/sra-vectr-runtime-8.9.2-ce.zip 
 unzip sra-vectr-runtime-8.9.2-ce.zip
+cp /home/vagrant/.env /opt/vectr
 
 # Install Vectr
 sudo docker compose up -d
