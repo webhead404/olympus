f968347b2a4f1a70afcb369dd2472823ef110677
webhead404 <kvvincentvalentine@gmail.com>
2024-11-11 16:16:51 -0600

    fix vectr, swap to yum

--- vagrant/scripts/elastic/start-ecp.sh
+++ vagrant/scripts/elastic/start-ecp.sh
@@ -1,4 +1,6 @@
+cp /home/vagrant/.env /home/vagrant/elastic-container/
 cd /home/vagrant/elastic-container
 chown -R vagrant: /home/vagrant/elastic-container
 chmod +x /home/vagrant/elastic-container/elastic-container.sh
+dos2unix  /home/vagrant/elastic-container/.env
 ./elastic-container.sh start
