82e880f7c3530b32b4edc32490f573a6a37db61b
webhead404 <kvvincentvalentine@gmail.com>
2024-11-12 16:05:10 -0600

    restarting ECP for test

--- vagrant/scripts/elastic/start-ecp.sh
+++ vagrant/scripts/elastic/start-ecp.sh
@@ -4,3 +4,23 @@ chown -R vagrant: /home/vagrant/elastic-container
 chmod +x /home/vagrant/elastic-container/elastic-container.sh
 dos2unix  /home/vagrant/elastic-container/.env
 ./elastic-container.sh start
+
+# Restarting ECP
+./elastic-container.sh restart
+
+check_fleet_ready() {
+  response=$(curl -s -k -u elastic:vagrant https://127.0.0.1:5601/api/fleet/agents/setup)
+  is_ready=$(echo "$response" | jq -r '.isReady')
+}
+
+while true; do
+  check_fleet_ready
+  
+  if [ "$is_ready" = "true" ]; then
+    echo "Fleet Server is ready."
+    exit 0
+  else
+    echo "Fleet Server is not ready. Retrying in 10 seconds..."
+    sleep 10
+  fi
+done
